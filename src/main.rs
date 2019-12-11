a959c574bdae9459ee07d9c40aacbe22087a54da
DasCapschen <3923406+DasCapschen@users.noreply.github.com>
2019-12-11 15:19:04 +0100

    add inputs

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,6 @@ use crate::sphere::Sphere;
 use crate::vec3::Vec3;
 
 use std::time::SystemTime;
-use sdl2::rect::Rect;
-use sdl2::pixels::Color;
 
 mod camera;
 mod hittable;
@@ -26,17 +24,21 @@ const WIDTH: u32 = 1280;
 const HEIGHT: u32 = 720;
 
 fn main() {
+    //initialise SDL2
     let sdl2_context = sdl2::init().unwrap();
     let video_subsystem = sdl2_context.video().unwrap();
 
+    //create a window
     let window = video_subsystem
         .window("Raytracer", WIDTH, HEIGHT)
         .position_centered()
         .build()
         .unwrap();
 
-    let mut renderer = Renderer::new((WIDTH/2) as i32, (HEIGHT/2) as i32, 64);
+    //create the actual raytracer
+    let mut renderer = Renderer::new((WIDTH/2) as i32, (HEIGHT/2) as i32, 1);
 
+    //create some materials
     let ground_sphere_mat = Material::new(Vec3::rgb(100, 200, 30), 0.0, 1.0);
     let diffuse_sphere_mat = Material::new(Vec3::rgb(200, 75, 75), 0.0, 1.0);
     let sphere1_mat = Material::new(Vec3::rgb(200, 200, 200), 1.0, 1.0);
@@ -68,11 +70,13 @@ fn main() {
         material: ground_sphere_mat,
     }));
 
+    //create a light
     renderer.add_light(Light::new(
         Vec3::new(1.0, 2.0, -2.0),
         Vec3::new(255.0, 255.0, 255.0),
     ));
 
+    //main loop
     let mut event_pump = sdl2_context.event_pump().unwrap();
     'running: loop {
         //check events (resize, quit, buttons, ...)
@@ -83,6 +87,17 @@ fn main() {
                     keycode: Some(Keycode::Escape),
                     ..
                 } => break 'running,
+                Event::MouseMotion{xrel, yrel, ..} => {
+                    let dx = xrel as f64 / WIDTH as f64;
+                    let dy = yrel as f64 / HEIGHT as f64;
+                    renderer.camera.direction += Vec3::new(dx, -dy, 0.0);
+                }
+                Event::KeyDown {keycode: Some(Keycode::W), ..} => renderer.camera.position += Vec3::new(0.0, 0.0, 0.1),
+                Event::KeyDown {keycode: Some(Keycode::S), ..} => renderer.camera.position += Vec3::new(0.0, 0.0, -0.1),
+                Event::KeyDown {keycode: Some(Keycode::D), ..} => renderer.camera.position += Vec3::new(0.1, 0.0, 0.0),
+                Event::KeyDown {keycode: Some(Keycode::A), ..} => renderer.camera.position += Vec3::new(-0.1, 0.0, 0.0),
+                Event::KeyDown {keycode: Some(Keycode::Space), ..} => renderer.camera.position += Vec3::new(0.0, 0.1, 0.0),
+                Event::KeyDown {keycode: Some(Keycode::C), ..} => renderer.camera.position += Vec3::new(0.0, -0.1, 0.0),
                 _ => {}
             }
         }
@@ -91,18 +106,16 @@ fn main() {
         let start_time = SystemTime::now();
         let pixels = renderer.draw_image();
         let end_time = SystemTime::now();
-        println!("DRAW! ({:?})", end_time.duration_since(start_time).unwrap());
+        //println!("DRAW! ({:?})", end_time.duration_since(start_time).unwrap());
 
-        //get surface
         let mut surface = window.surface(&event_pump).unwrap();
 
         //write pixels
         if let Some(pixel_buffer) = surface.without_lock_mut() {
-
             pixel_buffer.copy_from_slice(pixels);
         }
 
-        //"swap"
+        //"swap" images
         surface.update_window().expect("failed to update windows!");
     }
 }
