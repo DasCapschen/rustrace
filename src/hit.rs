a3bec395beaf0977d0957ec9b3022071703d7340
DasCapschen <3923406+DasCapschen@users.noreply.github.com>
2020-01-29 10:54:33 +0100

    impl Hit for Arc<dyn Hit>

--- src/hit.rs
+++ src/hit.rs
@@ -20,61 +20,6 @@ pub trait Hit: Send + Sync {
     fn center(&self) -> Vec3;
 }
 
-// hit a list of any hittables
-// useful for hitting world (all objects) in renderer
-impl Hit for Vec<Arc<dyn Hit>> {
-    fn hit(&self, ray: &Ray, t_min: f32, t_max: f32) -> Option<HitResult> {
-        let mut closest = t_max;
-        let mut result = None;
-
-        for object in self {
-            if let Some(hit) = object.hit(ray, t_min, closest) {
-                closest = hit.ray_param;
-                result = Some(hit);
-            }
-        }
-
-        result
-    }
-
-    fn bounding_box(&self) -> Option<AABB> {
-        //don't run if no objects!
-        if self.is_empty() {
-            return None;
-        }
-
-        //if only 1 object, return its bb
-        if self.len() == 1 {
-            return self[0].bounding_box();
-        }
-        //else...
-
-        //get bounding box of first object
-        if let Some(mut bb) = self.first().unwrap().bounding_box() {
-            //now, for every other object, get its bounding box
-            for obj in self {
-                if let Some(bb2) = obj.bounding_box() {
-                    //and then make a new bounding box containing both bounding boxes!
-                    bb = AABB::surrounding_box(&bb, &bb2);
-                } else {
-                    //if something has no bounding box, we don't have a bounding box at all!
-                    return None;
-                }
-            }
-
-            //return bb containing all bbs.
-            Some(bb)
-        } else {
-            //if first object has no bb, no bb at all!
-            None
-        }
-    }
-
-    fn center(&self) -> Vec3 {
-        todo!()
-    }
-}
-
 impl Hit for [Arc<dyn Hit>] {
     fn hit(&self, _ray: &Ray, _t_min: f32, _t_max: f32) -> Option<HitResult> {
         None
@@ -107,9 +52,7 @@ impl Hit for [Arc<dyn Hit>] {
 
 //hit a list of specific hittable
 //useful for hitting triangles of a mesh
-impl<T> Hit for Vec<T>
-where
-    T: Hit,
+impl<T: Hit> Hit for Vec<T>
 {
     fn hit(&self, ray: &Ray, t_min: f32, t_max: f32) -> Option<HitResult> {
         let mut closest = t_max;
@@ -160,5 +103,18 @@ where
 
     fn center(&self) -> Vec3 {
         todo!()
+    }
+}
+
+/// simply calls Hit on the object in the Arc
+impl Hit for Arc<dyn Hit> {
+    fn hit(&self, ray: &Ray, t_min: f32, t_max: f32) -> Option<HitResult> {
+        self.as_ref().hit(ray, t_min, t_max)
+    }
+    fn bounding_box(&self) -> Option<AABB> {
+        self.as_ref().bounding_box()
+    }
+    fn center(&self) -> Vec3 {
+        self.as_ref().center()
     }
 }
