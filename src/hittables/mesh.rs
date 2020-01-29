6e99d62681fba53707abee6d98b0d8fd53eb915c
DasCapschen <3923406+DasCapschen@users.noreply.github.com>
2020-01-29 10:54:18 +0100

    mesh loading (bvh broken)

--- src/hittables/mesh.rs
+++ src/hittables/mesh.rs
@@ -1,28 +1,77 @@
+use crate::gfx::material::Material;
+use crate::gfx::material::Metallic;
+use crate::gfx::texture::ConstantTexture;
 use std::path::Path;
+use std::sync::Arc;
+use std::str::FromStr;
 
 use crate::hit::{Hit, HitResult};
 use crate::hittables::aabb::AABB;
-use crate::hittables::primitives::Triangle;
+use crate::hittables::primitives::Plane;
 use crate::math::vec3::Vec3;
 use crate::ray::Ray;
+use crate::hittables::bvh::BvhTree;
+
+use std::io::BufReader;
+use std::io::BufRead;
+use std::fs::File;
 
 #[derive(Clone)]
 pub struct Mesh {
     position: Vec3,
-    faces: Vec<Triangle>,
-    bounding_box: Option<AABB>,
+    faces: BvhTree<Plane>,
+    //faces: Vec<Plane>
 }
 
 impl Mesh {
-    pub fn new(_file: &Path) -> Self {
-        //TODO: load file
-        let faces = Vec::new();
-        let bounding_box = faces.bounding_box();
+    pub fn new<P: AsRef<Path>>(file: P) -> Self {
+        let (models, mats) = tobj::load_obj(file.as_ref()).expect("couldn't load file");
+
+        //load material
+        let material = Arc::new(Material::new(
+                Arc::new(ConstantTexture::new(Vec3::new(0.9, 0.9, 0.9))),
+                None,
+                Metallic::NonMetal,
+                None,
+        ));
+
+        // just assume there is only 1 model in the obj!
+        let mesh: Vec<Plane> = models[0].mesh.indices
+            .chunks(3)
+            .map(|chunk| {
+                let v1 = Vec3 {
+                    x: models[0].mesh.positions[ 3 * chunk[0] as usize + 0 ],
+                    y: models[0].mesh.positions[ 3 * chunk[0] as usize + 1 ],
+                    z: models[0].mesh.positions[ 3 * chunk[0] as usize + 2 ],
+                };
+
+                let v2 = Vec3 {
+                    x: models[0].mesh.positions[ 3 * chunk[1] as usize + 0 ],
+                    y: models[0].mesh.positions[ 3 * chunk[1] as usize + 1 ],
+                    z: models[0].mesh.positions[ 3 * chunk[1] as usize + 2 ],
+                };
+
+                let v3 = Vec3 {
+                    x: models[0].mesh.positions[ 3 * chunk[2] as usize + 0 ],
+                    y: models[0].mesh.positions[ 3 * chunk[2] as usize + 1 ],
+                    z: models[0].mesh.positions[ 3 * chunk[2] as usize + 2 ],
+                };
+
+                Plane {
+                    llc: v1,
+                    span_a: v2 - v1,
+                    span_b: v3 - v1,
+                    material: material.clone(),
+                    triangle: true,
+                }
+            })
+            .collect();
+        
+        let bvh = BvhTree::from_hittables(mesh);
 
         Mesh {
             position: Vec3::new(0.0, 0.0, 0.0),
-            faces,
-            bounding_box,
+            faces: bvh,
         }
     }
 }
@@ -47,7 +96,7 @@ impl Hit for Mesh {
     }
 
     fn bounding_box(&self) -> Option<AABB> {
-        self.bounding_box
+        self.faces.bounding_box()
     }
 
     fn center(&self) -> Vec3 {
